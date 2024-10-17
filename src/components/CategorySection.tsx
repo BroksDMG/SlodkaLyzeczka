0a68016d7991bc67add8c95031bcdcf94e622591
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-10-17 14:09:14 +0200

    infinite moving animation of category card done

--- src/components/CategorySection.tsx
+++ src/components/CategorySection.tsx
@@ -1,30 +1,66 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import CategoryCard from "./CategoryCard";
 import theme from "@/styles/theme";
-import React from "react";
-const CategorySectionContainer=styled.section`
-    width:100%;
-    height:100vh;
-    position:relative;
-    overflow:hidden;
-    display: flex;
-    align-items:center;
-    overflow-x: auto; /* Allow horizontal scrolling */
-`
-interface CategorySectionInterface{
-    setOnClickCategoryHandler:(id:string)=>void;
+import React, { useRef } from "react";
+
+// Tworzymy animację przesuwania w nieskończoność
+const scroll = keyframes`
+  0% { transform: translateX(0); }
+  100% { transform: translateX(-50%); }
+`;
+// Kontener sekcji slidera
+const CategorySectionContainer = styled.section`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+`;
+// Kontener elementów slidera
+const SliderWrapper = styled.div`
+  display: flex;
+  width: max-content;
+  animation: ${scroll} 40s linear infinite;
+  animation-play-state: running;
+  
+  &:hover {
+    animation-play-state: paused;
+    transition: all 0.5s ease; 
+  } 
+`;
+
+interface CategorySectionInterface {
+  setOnClickCategoryHandler: (id: string) => void;
 }
 
-const CategorySection:React.FC<CategorySectionInterface>=({setOnClickCategoryHandler})=>{
+const CategorySection: React.FC<CategorySectionInterface> = ({ setOnClickCategoryHandler }) => {
 
-  return(
-    <CategorySectionContainer>
-      {qualityData.map((data,i)=><CategoryCard setOnClickCategoryHandler={setOnClickCategoryHandler} id={data.id} image={data.image} key={data.image+i} description={data.description} title={data.title} color={data.color}/>)}
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <CategorySectionContainer
+    >
+      <SliderWrapper ref={sliderRef}>
+        {[...qualityData, ...qualityData].map((data, i) => (
+          <>{/* Powielamy elementy dla nieskończonego efektu */}
+            {console.log(qualityData)}
+            <CategoryCard
+              setOnClickCategoryHandler={setOnClickCategoryHandler}
+              id={data.id}
+              image={data.image}
+              key={data.image + i}
+              description={data.description}
+              title={data.title}
+              color={data.color}
+            /></>
+        ))}
+      </SliderWrapper>
     </CategorySectionContainer>
-  )
-}
+  );
+};
 
-export default CategorySection
+export default CategorySection;
 
 const qualityData=[
   {
