85ea2e1d411615dbecdbb50f0dc1982bf6ee156a
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2024-11-08 23:26:10 +0100

    animation almost done

--- src/components/StageSlider.tsx
+++ src/components/StageSlider.tsx
@@ -36,6 +36,8 @@ const StageCard = styled.div<{ setCurrentStageVideo:(video:string)=>void }>`
   aspect-ratio:24/41;
   cursor: pointer;
   opacity:0.5;
+  transition: transform 0.4s ease; /* Płynna animacja */
+
 `;
 const StageCardImage= styled(Image)`
 object-fit:cover;
@@ -55,6 +57,7 @@ const StageContainer = styled.div`
   justify-content: end;
   align-items: end;
   height: 100vh;
+
   .slider {
     width: 50%;
     height:35vh;
@@ -62,48 +65,55 @@ const StageContainer = styled.div`
     gap: ${props => props.theme.spacing.medium};
     margin-bottom: ${props => props.theme.spacing.xlarge};
     /* transform:translateX(10em); */
+
     &.next {
-      transform:translateX(10em);
-      animation: ${moveStages} 0.3s linear 0.3s forwards;
+      /* transform:translateX(10em);
+      animation: ${moveStages} 0.3s linear 0.3s forwards; */
+
       .stageCard:nth-child(1) {
-        animation: ${showStage} 0.4s linear;
+        animation: ${showStage} 0.4s linear ;
         z-index: 100;
       }
-      .stageCard:nth-last-child(1) {
+
+      /* .stageCard:nth-last-child(1) {
         width: 0;
         animation: ${showlastStage} 0.3s linear 0.3s forwards;
-      }
+      } */
     }
   }
 `;
 const StageSlider: React.FC<StageCardProps> = ({  setCurrentStageVideo, }) => {
   const [stagesData,setStagesData]=useState([
+    { imagePath: '/images/Stage0.jpg', videoPath: '/videos/main.mp4' },
     { imagePath: '/images/Stage1.jpg', videoPath: '/videos/Ciastko.mp4' },
     { imagePath: '/images/Stage2.jpg', videoPath: '/videos/Babeczki.mp4' },
-    { imagePath: '/images/Stage0.jpg', videoPath: '/videos/main.mp4' },
   ])
 
-  const [activeIndex, setActiveIndex] = useState(stagesData.length-1);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const handleSceneChange = (newVideo: string, index: number) => {
     setCurrentStageVideo(newVideo);
     setActiveIndex(index);
   };
-
+  console.log(activeIndex)
   const buttonHandler = (direction  : number) => {
     const newIndex = (activeIndex + direction + stagesData.length) % stagesData.length;
-    setCurrentStageVideo(stagesData[newIndex].videoPath);
-    setActiveIndex(newIndex);
+    setTimeout(()=>{
+      setCurrentStageVideo(stagesData[newIndex].videoPath);
+      setActiveIndex(newIndex);
+    },400)
+
     const sliderDom= document.querySelector('.slider');
     const itemThumbNail= document.querySelectorAll('.stageCard');
+
     if(direction===1){
+
       sliderDom?.classList.add('next')
+
       setTimeout(() => {
-        sliderDom?.appendChild(itemThumbNail[0]);
+        sliderDom?.appendChild(itemThumbNail[0]) ;
         sliderDom?.classList.remove('next');
-        // Przesunięcie pierwszego elementu na koniec po zakończeniu animacji
-        // sliderDom.appendChild(sliderDom.querySelector('.stageCard'));
-      }, 600); // Czas trwania animacji + mała przerwa
+      }, 1700); // Czas trwania animacji + mała przerwa
     }
   };
   return(
